4e02d3dff5dd67eae0d7b1eea6fbb3c156b82815
Lewis Cramb <lewiscramb@icloud.com>
2026-01-15 15:20:42 +0000

    Chapter 4.1 complete

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(rqst):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    #Construct dict to pass template engine as context
+    #boldmessage matches to {{boldmessage}}
+    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #return rendered repsonse
+    #shortcut function
+    #first param is template to use
+    return render(rqst, 'rango/index.html', context=context_dict)
 
 def about(rqst):
     return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
