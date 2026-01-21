6f33c2a433455dbd7ffd77920169c7431835d0ba
Lewis Cramb <lewiscramb@icloud.com>
2026-01-21 08:01:32 +0000

    Chapter 6.2 complete

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(rqst):
-    #Construct dict to pass template engine as context
-    #boldmessage matches to {{boldmessage}}
-    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
-
-    #return rendered repsonse
-    #shortcut function
-    #first param is template to use
+    #loop through cateogries, sorted by likes, and get top 5
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
     return render(rqst, 'rango/index.html', context=context_dict)
 
 def about(rqst):
